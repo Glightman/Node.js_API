3d2ef70ba1080cd29fbee132a64f20cf24ee55e1
Gabriel Santana <gabriellima36716@gmail.com>
2022-07-20 12:23:56 -0300

    Update AuthController.js

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,11 +4,23 @@ const UserModel = require("../models/User");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+router.post("/register", async(req, res) => {
+    const {email} = req.body;
+    if(await UserModel.findOne({email})){
+        return res.status(400).json({
+            error: true,
+            message: "Usuário já existe com este email!"
+        })
+    }
+
+    const User = await UserModel.create(req.body);
+
+    User.password = undefined;
     console.log(req.body);
     return res.json({
         error: false,
-        message: "Resgistrado com sucesso!"
+        message: "Resgistrado com sucesso!",
+        data: User
     });
 })
 
