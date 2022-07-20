0c84b1a441e281d0f5374fc17ad9850203886ee7
Gabriel Santana <gabriellima36716@gmail.com>
2022-07-20 13:59:08 -0300

    implementação do JWT Authetication

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
 
 const UserModel = require("../models/User");
 
@@ -22,6 +25,41 @@ router.post("/register", async(req, res) => {
         message: "Resgistrado com sucesso!",
         data: User
     });
+})
+
+router.post("/authenticate", async(req, res) => {
+    const {email, password} = req.body;
+    
+    const user = await UserModel.findOne({email}).select("+password");
+
+    if(!user){
+        return res.status(400).json({
+            error: true,
+            message: "Usuário não encontrado!"
+        })
+    }
+
+    if(!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({
+            error: true,
+            message: "Senha incorreta!"
+        })
+    }
+
+    user.password = undefined;
+
+    const token = jwt.sign({
+        id: user.id,
+        name: user.name
+    }, authConfig.secret, {
+        expiresIn: 86400
+    });
+    
+    return res.json({
+        user,
+        token
+    });
+    
 })
 
 module.exports = router;
